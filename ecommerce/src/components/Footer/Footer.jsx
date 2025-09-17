982a2428120a40568ae22fdc24b98df626aa4940
kunalpanchal882 <panchalkunal051@gmail.com>
2025-09-17 13:46:31 +0530

    footer-added

--- ecommerce/src/components/Footer/Footer.jsx
+++ ecommerce/src/components/Footer/Footer.jsx
@@ -1,111 +1,140 @@
-import React from 'react'
-import style from './Footer.module.css'
+import React from "react";
+import style from "./Footer.module.css";
 import { RiChatSmileAiFill } from "react-icons/ri";
-import { FaFacebook } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa6";
+import { SlSocialFacebook } from "react-icons/sl";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { IoLogoInstagram } from "react-icons/io5";
 import { IoLocationOutline } from "react-icons/io5";
 import { IoCallSharp } from "react-icons/io5";
 import { MdMarkEmailRead } from "react-icons/md";
-import { BsEmojiSmile } from "react-icons/bs";
-const Footer = () => {
 
+const Footer = () => {
   const Links = [
     {
-      "text":"About US"
+      text: "About US",
     },
     {
-      "text":"How It Works"
+      text: "How It Works",
     },
     {
-      "text":"Trade Assurance"
+      text: "Trade Assurance",
     },
     {
-      "text":"Success Stories"
+      text: "Success Stories",
     },
     {
-      "text":"Press & media"
+      text: "Press & media",
     },
     {
-      "text":"Careers"
+      text: "Careers",
     },
-  ]
+  ];
 
   const Catogories = [
     {
-      "text":"Textile & Apparel"
+      text: "Textile & Apparel",
     },
     {
-      "text":"Electronics"
+      text: "Electronics",
     },
     {
-      "text":"Machinery"
+      text: "Machinery",
     },
     {
-      "text":"Chemicals"
+      text: "Chemicals",
     },
     {
-      "text":"Construction"
+      text: "Construction",
     },
-  ]
+  ];
 
   const contact = [
     {
-      "text":"ghaziabad",
-      "icons":<IoLocationOutline />
+      text: "ghaziabad",
+      icons: <IoLocationOutline />,
     },
     {
-      "text":"+91 100-123-4547",
-      "icons":<IoCallSharp />
+      text: "+91 100-123-4547",
+      icons: <IoCallSharp />,
     },
     {
-      "text":"support@emojija.com",
-      "icons":<MdMarkEmailRead />
+      text: "support@emojija.com",
+      icons: <MdMarkEmailRead />,
     },
-  ]
-  
+  ];
+
+  const policy = [
+    {
+      text: "Privacy Policy",
+    },
+    {
+      text: "Terms of Services",
+    },
+    {
+      text: "Cookie Policy",
+    },
+  ];
+
   return (
-    <main className={style.mainFooter}>
-      <section className={style.linkSection}>
-        <div className={style.iconsTags}>
-        
+    <main className={style.mainContainer}>
+      <div className={style.mainFooter}>
+        <section className={style.linkSection}>
+          <div className={style.iconsTags}>
             <div className={style.imagelogo}>
-              <img src="../../../assists/WhatsApp_Image_2025-09-16_at_17.10.43_d3e706c2_processed_by_imagy-removebg-preview.png" alt="" />
-            <p>B2B MARKET PLACE</p>
+              <img
+                src="../../../assists/Gemini_Generated_Image_elyp5nelyp5nelyp__1_-removebg-preview.png"
+                alt=""
+              />
+              <span>B2B MARKET PLACE</span>
             </div>
-        </div>
-        <div >
-          <p>Empowering businesses with seamless B2B trade solutions, secure payments, and trusted partnerships. Connect with verified suppliers, streamline procurement, and grow globally with our reliable e-commerce platform.</p>
-          <div className={style.linkIcons}>
-              <FaFacebook className={style.icons}/>
-              <FaLinkedin className={style.icons}/>
-              <FaInstagram className={style.icons}/>
           </div>
-        </div>
-      </section>
-      <section className={style.aboutSection}>
-        <h2>Quick Links</h2>
+          <div>
+            <span>
+              Empowering businesses with seamless B2B trade solutions, secure
+              payments, and trusted partnerships. Connect with verified
+              suppliers, streamline procurement, and grow globally with our
+              reliable e-commerce platform.
+            </span>
+            <div className={style.linkIcons}>
+              <SlSocialFacebook className={style.icons} />
+              <SlSocialLinkedin className={style.icons} />
+              <IoLogoInstagram className={style.icons} />
+            </div>
+          </div>
+        </section>
+        <section className={style.aboutSection}>
+          <h2>Quick Links</h2>
           {Links.map((link) => (
             <p>{link.text}</p>
           ))}
-      </section>
-      <section className={style.catogoriesSection}>
-        <h2>Popular Categories</h2>
-        {Catogories.map((cat) => (
-          <p>{cat.text}</p>
-        ))}
-      </section>
-      <section className={style.contectSection}>
-        <h2>Contact Us</h2>
-        {contact.map((con) => (
-         <div className={style.contact}> 
-          <p>{con.icons}</p>
-          <p>{con.text}</p>
-          </div>
-        ))}
+        </section>
+        <section className={style.catogoriesSection}>
+          <h2>Popular Categories</h2>
+          {Catogories.map((cat) => (
+            <p>{cat.text}</p>
+          ))}
+        </section>
+        <section className={style.contectSection}>
+          <h2>Contact Us</h2>
+          {contact.map((con) => (
+            <div className={style.contact}>
+              <p>{con.icons}</p>
+              <p>{con.text}</p>
+            </div>
+          ))}
+        </section>
+      </div>
+      <hr className={style.line}/>
+      <section className={style.lastSection}>
+        <span>@2025 emojija.com. All right reserved</span>
+        <div className={style.policy}>
+          {policy.map((policy) => (
+            <p>{policy.text}</p>
+          ))}
+        </div>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
