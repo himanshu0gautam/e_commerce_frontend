ae9d39c774280205be0bb0bbec771b4141aedf41
Hardik Sharma <kumarankit2002dseu@gmail.com>
2025-09-17 15:48:51 +0530

    merging all categories

--- ecommerce/src/Route/Route.jsx
+++ ecommerce/src/Route/Route.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
         children : [
             {
                 index : true,
-                element: <AllCategories />
+                element: <Home />
             },
             {
                 path:'jewellery',
