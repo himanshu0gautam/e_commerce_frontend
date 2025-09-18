de3e5b19817dd96370bddf0c1efd23ba488e7fcf
Hardik Sharma <kumarankit2002dseu@gmail.com>
2025-09-18 17:53:24 +0530

    added prefooter

--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Carousel from '../components/Carousel/Carousel'
+import PrefFooter from '../components/preFooter/PrefFooter'
 import CategorySection from './CategorySection'
 const Home = () => {
   return (
     <div>
       <Carousel/>
       <CategorySection/>
+      <PrefFooter/>
     </div>
   )
 }
