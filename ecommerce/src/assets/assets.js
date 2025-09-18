6796bae179f7d2acbf6f955ccbc176e956e51b00
Hardik Sharma <kumarankit2002dseu@gmail.com>
2025-09-18 12:52:34 +0530

    corousel done

--- ecommerce/src/assets/assets.js
+++ ecommerce/src/assets/assets.js
@@ -1,9 +1,13 @@
 import Logo from '../assets/Logo.png'
 import search from '../assets/Search.png'
-
+import Carousel1 from '../assets/Carousel1.jpg'
+import Carousel2 from '../assets/Carousel2.jpg'
+import Carousel3 from '../assets/Carousel3.png'
 export const  assets = {
     Logo,
     search,
-   
+    Carousel1,
+    Carousel2,
+    Carousel3
   
 }
