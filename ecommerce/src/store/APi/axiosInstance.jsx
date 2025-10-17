1047e4423e4d9fc34d6eb327898db2e5afabe4d7
Hardik Sharma <kumarankit2002dseu@gmail.com>
2025-10-17 11:47:49 +0530

    added settings

--- ecommerce/src/store/APi/axiosInstance.jsx
+++ ecommerce/src/store/APi/axiosInstance.jsx
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-<<<<<<< HEAD
     baseURL:"http://localhost:3000/api",
-=======
-    baseURL:"http://192.168.1.58:3000/api",
->>>>>>> kunal
+    // baseURL:"http://192.168.1.58:3000/api",
     withCredentials:true
 })
 
