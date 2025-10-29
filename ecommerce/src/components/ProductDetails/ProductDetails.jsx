227494ba14f1edb0e56263ce9df519d8b7976d7d
Hardik Sharma <kumarankit2002dseu@gmail.com>
2025-10-29 16:33:00 +0530

    added cart

--- ecommerce/src/components/ProductDetails/ProductDetails.jsx
+++ ecommerce/src/components/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useParams, useLocation } from 'react-router-dom'
 import styles from './ProductDetails.module.css'
+import ImageZoom from '../ImageZoom/ImageZoom'
 const ProductDetails = () => {
     const location = useLocation()
     const product = location.state?.pdt;
@@ -22,17 +23,31 @@ const ProductDetails = () => {
 
                         
                             <div className={styles.rightImg}>
-                                <img src={product.image[currindx]} alt="" height={470} width={470} />
+                                <ImageZoom src={product.image[currindx]} alt="" height={470} width={470} />
                             </div>
                         
                    
                 </div>
             </div>
 
-            <div className={styles.rightCont}>
-                <div className={styles.Heading}>{product?.title}{product?.Name}</div>
-                <div>{product?.rating}</div>
-                <div>{product?.price}</div>
+           <div className={styles.rightCont}>
+            <div className={styles.heading}>
+                {product?.title} <span className={styles.brand}>({product?.Name})</span>
+            </div>
+
+            <div className={styles.rating}>
+                ⭐ <span>{product?.rating}</span> / 5
+            </div>
+
+            <div className={styles.price}>
+                {product?.price?.toLocaleString()} 
+                <span className={styles.tax}> &nbsp;Incl. of all taxes</span>
+            </div>
+
+            <div className={styles.actions}>
+                <button className={styles.addToCart}>Add to Cart</button>
+                <button className={styles.buyNow}>Buy Now</button>
+            </div>
             </div>
 
         </div>
